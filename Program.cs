e7444f8a8281da2c1d8483771f0c68bfd0663517
biplovknwr <169064487+biplovknwr@users.noreply.github.com>
2025-02-23 22:01:33 -0500

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+
+using _200604735.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<StudentService>();
 
 var app = builder.Build();
 
